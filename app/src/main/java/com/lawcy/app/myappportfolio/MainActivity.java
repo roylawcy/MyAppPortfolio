81cf43f0f8cc35c4ee09fcffe9eb268f6f130c54
Roy Law <lawchoyin@gmail.com>
2015-07-19 02:26:19 +0800

    Change to AppCompatActivity

--- app/src/main/java/com/lawcy/app/myappportfolio/MainActivity.java
+++ app/src/main/java/com/lawcy/app/myappportfolio/MainActivity.java
@@ -1,15 +1,14 @@
 package com.lawcy.app.myappportfolio;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mSpotifyStreamerButton;
     private Button mScoreAppButton;
